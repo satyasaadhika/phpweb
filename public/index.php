b4564e769f83b2a19b5f2b758e1a80e450065621
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2024-08-11 09:13:38 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -14,8 +14,6 @@ $data = json_decode(file_get_contents(__DIR__ . "/assets/datas/data.json"), true
 </head>
 <body>
     <h1>Priyanshu Maurya PHP</h1>
-    <p>The SHA256 hash of "Priyanshu Maurya" is
-    087f671aa83511d9221dff5c0cb1c9bb5215bfca7f65e042865cc7bcb44e46be</p>
     <p><?= $text ?></p>
     <pre>
     ASCII ART:
@@ -33,18 +31,12 @@ $data = json_decode(file_get_contents(__DIR__ . "/assets/datas/data.json"), true
         **
     </pre>
     <?php
-    echo "SHA256: " . hash('sha256', "Priyanshu Maurya");
+      <p>The SHA256 hash of "Priyanshu Maurya" is  echo "SHA256: " . hash('sha256', "Priyanshu Maurya");</p>
     ?>
     <br/>   
     <br/> 
    <a href="/public/assets/check.php">Click here to check the error setting</a>
    <br/>
     <a href="/public/assets/fail.php">Click here to cause a traceback</a>
-    <?php foreach ($data as $paragraph) : ?>
-        <div>
-            <h2><?= $paragraph['title'] ?></h2>
-            <p><?= $paragraph['text'] ?></p>
-        </div>
-    <?php endforeach ?>
 </body>
 </html>
